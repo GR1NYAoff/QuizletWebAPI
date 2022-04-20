6816c5e29af88eec1d14b3e3bb1e05781e7356b6
= <=>
2022-04-20 15:34:40 +0300

    add models

---
+++ QuizletWebAPI.Resourse/Models/Answer.cs
@@ -0,0 +1,9 @@
+﻿namespace QuizletWebAPI.Resourse.Models
+{
+    public class Answer
+    {
+        public string First { get; set; } = "Yes";
+        public string Second { get; set; } = "No";
+        public string Third { get; set; } = "Idk";
+    }
+}
---
+++ QuizletWebAPI.Resourse/Models/Question.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizletWebAPI.Resourse.Models
+{
+    public class Question
+    {
+        public int Number { get; set; }
+        public string Body { get; set; } = string.Empty;
+    }
+}
