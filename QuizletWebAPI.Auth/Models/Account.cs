6b65c90c49406dbdf6666c1d156ae6066a86dffd
= <=>
2022-04-20 12:23:01 +0300

    add DTO (Account)

---
+++ QuizletWebAPI.Auth/Models/Account.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuizletWebAPI.Auth.Models
+{
+    public class Account
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Login { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
