56b4b464943faadf9b75cb240f2c5b08055060be
= <=>
2022-04-20 13:30:29 +0300

    add AuthController

---
+++ QuizletWebAPI.Auth/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizletWebAPI.Auth.Data;
+
+namespace QuizletWebAPI.Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public AuthController(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
