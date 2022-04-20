78753f5b5a4b8a9290d0ae6e1d572b4c585d97ce
= <=>
2022-04-20 13:44:48 +0300

    implemented Authenticate User and added AuthOptions in ctor

--- QuizletWebAPI.Auth/Controllers/AuthController.cs
+++ QuizletWebAPI.Auth/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using QuizletWebAPI.Auth.Data;
+using QuizletWebAPI.Auth.Models;
+using QuizletWebAPI.Common;
 
 namespace QuizletWebAPI.Auth.Controllers
 {
@@ -8,16 +11,31 @@ namespace QuizletWebAPI.Auth.Controllers
     public class AuthController : ControllerBase
     {
         private readonly DataContext _context;
-        public AuthController(DataContext dataContext)
+        private readonly IOptions<AuthOptions> _options;
+
+        public AuthController(DataContext dataContext, IOptions<AuthOptions> options)
         {
             _context = dataContext;
+            _options = options;
         }
 
         [Route("login")]
         [HttpPost]
-        public IActionResult Login(string email, string password)
+        public IActionResult Login([FromBody] Login request)
         {
+            var user = AuthenticateUser(request.Email, request.Password);
+
+            if (user == null)
+                return Unauthorized();
+
+            // TODO: get JWT
             throw new NotImplementedException();
+
+        }
+
+        private Account? AuthenticateUser(string email, string password)
+        {
+            return _context.Accounts.SingleOrDefault(a => a.Email == email && a.Password == password);
         }
 
     }
