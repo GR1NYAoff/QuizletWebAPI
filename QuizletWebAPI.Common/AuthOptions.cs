0f088627fd0e587b86b06ebcd1934846cd946a4f
= <=>
2022-04-20 13:38:03 +0300

    add lib for authentication

---
+++ QuizletWebAPI.Common/AuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace QuizletWebAPI.Common
+{
+    public class AuthOptions
+    {
+        public string Issuer { get; set; } = string.Empty;
+        public string Audience { get; set; } = string.Empty;
+        public string Secret { get; set; } = string.Empty;
+        public int TokenLifeTime { get; set; }
+        public SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Secret));
+        }
+    }
+}
