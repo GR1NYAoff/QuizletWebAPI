c89d8dd6c6ec519b8c316c125372388daa533292
= <=>
2022-04-21 02:16:13 +0300

    add models

---
+++ QuizletWebAPI.Auth/Models/Access.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuizletWebAPI.Auth.Models
+{
+    public class Access
+    {
+        [Key]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string TestId { get; set; } = string.Empty;
+    }
+}
---
+++ QuizletWebAPI.Auth/Models/Question.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizletWebAPI.Auth.Models
+{
+    public class Question
+    {
+        public int Number { get; set; }
+        public string Body { get; set; } = string.Empty;
+    }
+}
