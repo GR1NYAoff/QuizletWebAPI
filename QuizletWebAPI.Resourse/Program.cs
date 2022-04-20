95d1a108cc6f937ff75c0381d977d15d71ffe060
= <=>
2022-04-20 15:35:46 +0300

    add DB connection using DefaultConnection string

--- QuizletWebAPI.Resourse/Program.cs
+++ QuizletWebAPI.Resourse/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using QuizletWebAPI.Resourse.Data;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TestsDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
 
 var app = builder.Build();
 
