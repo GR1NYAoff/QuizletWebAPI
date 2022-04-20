a8c974b7f47588662dc2bcd50017702d5444dfd3
= <=>
2022-04-20 15:33:55 +0300

    add DB context

---
+++ QuizletWebAPI.Resourse/Data/TestsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizletWebAPI.Resourse.Models;
+
+namespace QuizletWebAPI.Resourse.Data
+{
+    public class TestsDbContext : DbContext
+    {
+        public TestsDbContext(DbContextOptions<TestsDbContext> options) : base(options) { }
+
+        public DbSet<Test> Tests { get; set; }
+    }
+}
