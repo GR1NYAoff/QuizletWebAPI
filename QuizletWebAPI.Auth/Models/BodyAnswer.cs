ed615e10ac4673bf0ec061f0929dcd9bbed19076
= <=>
2022-04-28 19:57:13 +0300

    add extra model for answer (BodyAnswer)

---
+++ QuizletWebAPI.Auth/Models/BodyAnswer.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizletWebAPI.Auth.Models
+{
+    public class BodyAnswer
+    {
+        public string Text { get; set; } = string.Empty;
+        public bool IsCorrect { get; set; }
+    }
+}
